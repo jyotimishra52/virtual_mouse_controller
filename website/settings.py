de608cb47471af0652b96b3979316c481fb9af35
Mahesh Konar <90597653+mahesh021011@users.noreply.github.com>
2023-04-11 17:24:26 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2697gbia5%vm!41a)7@ck&7wbni01$lxw)&po*)f99q6t^%ndx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
