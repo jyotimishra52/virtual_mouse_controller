611a21dd1627ba38b9f69a03eab6877ea8dbefdf
Mahesh Konar <90597653+mahesh021011@users.noreply.github.com>
2023-04-11 17:42:47 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2697gbia5%vm!41a)7@ck&7wbni01$lxw)&po*)f99q6t^%ndx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
